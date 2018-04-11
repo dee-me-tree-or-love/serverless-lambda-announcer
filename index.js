60a55e90031b35838f85cf7654f5f3f755fbcc96
dmitrii <dmitry-orlov@hotmail.com>
2018-04-11 12:04:29 +0200

    added retrieving of the lambda information

--- index.js
+++ index.js
@@ -1,51 +1,33 @@
 'use strict';
 
+const LambdaInfo = require('./lib/lambda-info');
+
 class ServerlessPlugin {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
-
-    this.commands = {
-      welcome: {
-        usage: 'Helps you start your first Serverless plugin',
-        lifecycleEvents: [
-          'hello',
-          'world',
-        ],
-        options: {
-          message: {
-            usage:
-              'Specify the message you want to deploy '
-              + '(e.g. "--message \'My Message\'" or "-m \'My Message\'")',
-            required: true,
-            shortcut: 'm',
-          },
-        },
-      },
-    };
+    this.service = this.serverless.service;
+    this.provider = this.serverless.getProvider('aws');
+    this.servicePath = this.serverless.config.servicePath || '';
 
     this.hooks = {
-      'before:welcome:hello': this.beforeWelcome.bind(this),
-      'welcome:hello': this.welcomeUser.bind(this),
-      'welcome:world': this.displayHelloMessage.bind(this),
-      'after:welcome:world': this.afterHelloWorld.bind(this),
+      // 'before:deploy:deploy': this.debug.bind(this),
+      // 'after:deploy:deploy': this.debug.bind(this),
+      'after:aws:info:displayEndpoints': this.debug.bind(this),
+      // 'before:aws:deploy:finalize:cleanup': this.debug.bind(this),
     };
   }
 
-  beforeWelcome() {
-    this.serverless.cli.log('Hello from Serverless!');
-  }
-
-  welcomeUser() {
-    this.serverless.cli.log('Your message:');
-  }
-
-  displayHelloMessage() {
-    this.serverless.cli.log(`${this.options.message}`);
-  }
-
-  afterHelloWorld() {
-    this.serverless.cli.log('Please come again!');
+  debug() {
+    const sls = this.serverless;
+    return LambdaInfo.getLambdaInfo(sls)
+      .then((info) => {
+        sls.cli.log("quark")
+        sls.cli.consoleLog(info);
+      })
+      .catch((error) => {
+        sls.cli.log(error);
+      });
   }
 }
 
