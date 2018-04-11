3155003c0dcf409d0544cd646c59cb3e8fc33976
dmitrii <dmitry-orlov@hotmail.com>
2018-04-11 12:53:23 +0200

    add the hook call

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const request = require('request');
+
 const LambdaInfo = require('./lib/lambda-info');
 
 class ServerlessPlugin {
@@ -15,12 +17,41 @@ class ServerlessPlugin {
     };
   }
 
-  debug() {
+  /// http://requestbin.fullcontact.com/uo9xoouo
+  announce(info) {
+    const options = {
+      method: 'post',
+      body: info,
+      json: true,
+      url: 'http://requestbin.fullcontact.com/uo9xoouo'
+    }
+    return new Promise((resolve, reject) => {
+      request(options, (err, res) => {
+        if (err) {
+          reject(new Error('bla'));
+          return;
+        }
+        resolve(res);
+        return;
+      })
+    }
+    );
+  }
+
+  handle() {
     const sls = this.serverless;
     return LambdaInfo.getLambdaInfo(sls)
       .then((info) => {
         sls.cli.log("quark")
         sls.cli.consoleLog(info);
+        return info;
+      })
+      .then((info) => {
+        return this.announce(info);
+      })
+      .then((result) => {
+        sls.cli.log("quark")
+        sls.cli.consoleLog(result);
       })
       .catch((error) => {
         sls.cli.log(error);
